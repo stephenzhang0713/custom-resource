ba93f116793a7ed27d4d85f3ed1840d3dfc572f5
Han Zhang <Stephenzhang0713@outlook.com>
2023-07-29 23:37:38 +0800

    commit

--- pkg/apis/samplecrd/v1/register.go
+++ pkg/apis/samplecrd/v1/register.go
@@ -1,1 +1,30 @@
 package v1
+
+import (
+	"custom-resource/pkg/apis/samplecrd"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// GroupVersion is the identifier for the API which includes
+// the name of the group and the version of the API
+var SchemeGroupVersion = schema.GroupVersion{
+	Group:   samplecrd.GroupName,
+	Version: samplecrd.Version,
+}
+
+// addKnownTypes adds our types to the API scheme by registering
+// Network and NetworkList
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
+		&Network{},
+		&NetworkList{},
+	)
+
+	// register the type in the scheme
+	metav1.AddToGroupVersion(scheme, metav1.SchemeGroupVersion)
+	return nil
+}
